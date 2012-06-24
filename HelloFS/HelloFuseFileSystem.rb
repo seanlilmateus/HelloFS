9641141bb18e11ed3e9df9c823645e97d0a54d19
seanlilmateus <seanlilmateus@yahoo.de>
2012-06-24 11:27:48 +0200

    added a second File and a screenshot

--- HelloFS/HelloFuseFileSystem.rb
+++ HelloFS/HelloFuseFileSystem.rb
@@ -4,20 +4,24 @@
 #
 
 class HelloFuseFileSystem
+  MacRubyStr = "MacRuby rocks!\n"
   HelloStr = "Hello World From MacRuby!\n"
   HelloPath = "/hello.txt"
+  MacRubyPath = "/macruby.txt"
   
   def contentsOfDirectoryAtPath(path, error:error)
-    [HelloPath.lastPathComponent]
+    [HelloPath, MacRubyPath].map(&:lastPathComponent)
   end
   
   def contentsAtPath(path)
-    return HelloStr.dataUsingEncoding(NSUTF8StringEncoding) if path == HelloPath
-    nil
+    case path
+      when HelloPath then HelloStr.dataUsingEncoding(NSUTF8StringEncoding)
+      when MacRubyPath then MacRubyStr.dataUsingEncoding(NSUTF8StringEncoding)
+    end
   end
   
   def finderAttributesAtPath(path, error:error)
-    if path == HelloPath
+    if path == HelloPath or path == MacRubyPath
       file = NSBundle.mainBundle.pathForResource("hellodoc", ofType:"icns")
       return { KGMUserFileSystemCustomIconDataKey => NSData.dataWithContentsOfFile(file) }
     end
--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 this is an example on how to use MacFuse in MacRuby,
 to play with it, you will need to install the MacFuse Framework, or even better the MacFuse replacement [OSXFUSE](http://osxfuse.github.com/)
 
+![screenshot](https://github.com/seanlilmateus/HelloFS/blob/master/helloFS?raw=true "Screenshot")
+
+
 ##Licensing:
 
 this projekt is licensed under the [BSD license](http://www.opensource.org/licenses/bsd-license.php).
